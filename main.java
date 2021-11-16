22d8b9c2c0db62becd6cbabb35dcc4dff51c0b8b
Vilhelm13 <90401410+Vilhelm13@users.noreply.github.com>
2021-11-16 15:25:35 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main {
+
+
+
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the Trivia game of Trivia!!");
+        System.out.println("The way to play is very simple");
+        System.out.println("there will be a question, you will give an answer, if it's correct you get points.Wrong? no points");
+
+
+    }
+}
